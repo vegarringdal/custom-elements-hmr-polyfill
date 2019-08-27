c8dca914efc5798a97a8639833563d8986066d6a
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-08-27 18:24:48 +0200

    fix: defineCustomElement.ts file name

--- src/package/index.ts
+++ src/package/index.ts
@@ -1,7 +1,7 @@
 import { initCache } from './polyfill/hmrCache';
 import { overrideCustomElementDefine } from './polyfill/overrideCustomElementDefine';
 export { reflowDOM } from './utils/reflowDOM';
-export { defineCustomElement } from './decorator/xdefineCustomElement';
+export { defineCustomElement } from './decorator/defineCustomElement';
 export { onCustomElementChange } from './polyfill/onCustomElementChange';
 
 export function applyPolyfill() {
