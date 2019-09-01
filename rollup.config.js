629244b1c112311f0f1fa01a2c06f7a872a25ac4
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-01 02:51:46 +0200

    chore: update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,57 @@
-
 import {terser} from 'rollup-plugin-terser';
 
-export default {
+export default [{
   input: 'dist/ES6/index.js',
   output: {
-    file: './dist/custom-elements-hmr-pollyfill.js',
+    file: './dist/custom-elements-hmr-pollyfill.system.js',
+    format: 'system',
+  },
+  plugins: [
+    terser()
+  ]
+}, {
+  input: 'dist/ES6/index.js',
+  output: {
+    file: './dist/custom-elements-hmr-pollyfill.amd.js',
+    format: 'amd',
+  },
+  plugins: [
+    terser()
+  ]
+}, {
+  input: 'dist/ES6/index.js',
+  output: {
+    file: './dist/custom-elements-hmr-pollyfill.cjs.js',
+    format: 'cjs',
+  },
+  plugins: [
+    terser()
+  ]
+}, {
+  input: 'dist/ES6/index.js',
+  output: {
+    file: './dist/custom-elements-hmr-pollyfill.iife.js',
+    format: 'iife',
+  },
+  plugins: [
+    terser()
+  ]
+}, {
+  input: 'dist/ES6/index.js',
+  output: {
+    file: './dist/custom-elements-hmr-pollyfill.umd.js',
+    format: 'umd',
+  },
+  plugins: [
+    terser()
+  ]
+}, {
+  input: 'dist/ES6/index.js',
+  output: {
+    file: './dist/custom-elements-hmr-pollyfill.mjs',
     format: 'esm',
   },
   plugins: [
     terser()
   ]
-}
+}]
