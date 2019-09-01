dbe9152c8e70ef2db9f665fc9d6b8d8f1c930cbe
Vegar RIngdal <vegar.ringdal@gmail.com>
2019-09-01 02:57:24 +0200

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,54 @@
-import {terser} from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 
-export default [{
-  input: 'dist/ES6/index.js',
-  output: {
-    file: './dist/custom-elements-hmr-pollyfill.system.js',
-    format: 'system',
-  },
-  plugins: [
-    terser()
-  ]
-}, {
-  input: 'dist/ES6/index.js',
-  output: {
-    file: './dist/custom-elements-hmr-pollyfill.amd.js',
-    format: 'amd',
-  },
-  plugins: [
-    terser()
-  ]
-}, {
-  input: 'dist/ES6/index.js',
-  output: {
-    file: './dist/custom-elements-hmr-pollyfill.cjs.js',
-    format: 'cjs',
-  },
-  plugins: [
-    terser()
-  ]
-}, {
-  input: 'dist/ES6/index.js',
-  output: {
-    file: './dist/custom-elements-hmr-pollyfill.iife.js',
-    format: 'iife',
-  },
-  plugins: [
-    terser()
-  ]
-}, {
-  input: 'dist/ES6/index.js',
-  output: {
-    file: './dist/custom-elements-hmr-pollyfill.umd.js',
-    format: 'umd',
-  },
-  plugins: [
-    terser()
-  ]
-}, {
-  input: 'dist/ES6/index.js',
-  output: {
-    file: './dist/custom-elements-hmr-pollyfill.mjs',
-    format: 'esm',
-  },
-  plugins: [
-    terser()
-  ]
-}]
+export default [
+    {
+        input: 'dist/ES6/index.js',
+        output: {
+            file: './dist/custom-elements-hmr-pollyfill.system.js',
+            format: 'system'
+        },
+        plugins: [terser()]
+    },
+    {
+        input: 'dist/ES6/index.js',
+        output: {
+            file: './dist/custom-elements-hmr-pollyfill.amd.js',
+            format: 'amd'
+        },
+        plugins: [terser()]
+    },
+    {
+        input: 'dist/ES6/index.js',
+        output: {
+            file: './dist/custom-elements-hmr-pollyfill.cjs.js',
+            format: 'cjs'
+        },
+        plugins: [terser()]
+    },
+    {
+        input: 'dist/ES6/index.js',
+        output: {
+            name: 'customElementsHmrPolyfill',
+            file: './dist/custom-elements-hmr-pollyfill-iife.js',
+            format: 'iife'
+        },
+        plugins: [terser()]
+    },
+    {
+        input: 'dist/ES6/index.js',
+        output: {
+          name: 'customElementsHmrPolyfill',
+            file: './dist/custom-elements-hmr-pollyfill.umd.js',
+            format: 'umd'
+        },
+        plugins: [terser()]
+    },
+    {
+        input: 'dist/ES6/index.js',
+        output: {
+            file: './dist/custom-elements-hmr-pollyfill.mjs',
+            format: 'esm'
+        },
+        plugins: [terser()]
+    }
+];
