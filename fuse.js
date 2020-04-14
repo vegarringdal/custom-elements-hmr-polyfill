a636161e6c929d2de155425415b29aaaf09601af
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-14 16:40:01 +0200

    chore: travis and test-ci

--- fuse.js
+++ fuse.js
@@ -74,3 +74,37 @@ task('build', async () => {
         build(target);
     });
 });
+
+task('typecheck-package', () => {
+    const typeChecker = require('fuse-box-typechecker').TypeChecker({
+        tsConfig: './tsconfig.json',
+        basePath: './',
+        tsLint: './tslint.json',
+        name: 'typecheck and lint src and test folders',
+        shortenFilenames: true,
+        yellowOnLint: true,
+        throwOnGlobal: true,
+        throwOnSemantic: true,
+        throwOnTsLint: true,
+        throwOnSyntactic: true,
+        throwOnOptions: true,
+        tsConfigOverride: {
+            compilerOptions: {
+                rootDir: `./src/package`,
+                target: 'es2018',
+                module: 'es6',
+                lib: ['es2017', 'dom'],
+                emitDecoratorMetadata: true,
+                skipLibCheck: true,
+                sourceMap: true,
+                declaration: true,
+                importHelpers: true,
+                experimentalDecorators: true
+            },
+            exclude: ['dist', 'node_modules', 'src/sample', 'src/__tests__']
+        }
+    });
+
+    typeChecker.printSettings();
+    typeChecker.printOnly(typeChecker.inspectOnly());
+});
